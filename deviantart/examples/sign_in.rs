2ea868f6788ddbedacfb682aa6bf216d27b9f4ce
Nathaniel Daniel <nathaniel.daniel23@outlook.com>
2024-02-11 18:18:32 -0800

    Add sign in example

---
+++ deviantart/examples/sign_in.rs
@@ -0,0 +1,9 @@
+#[tokio::main]
+async fn main() {
+    let client = deviantart::Client::new();
+    
+    client
+        .sign_in("username", "password")
+        .await
+        .expect("failed to sign in");
+}
