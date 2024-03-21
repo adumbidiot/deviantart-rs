bbe84d72b5198cecd221fcafa8df532bdf866779
adumbidiot <adumbidiot@users.noreply.github.com>
2024-03-21 00:50:27 +0000

    deploy: 024256abcec3256ca5e746657d434614466e93a5

---
+++ trait.impl/argh/trait.TopLevelCommand.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"deviantart_cli":[["impl TopLevelCommand for <a class=\"struct\" href=\"deviantart_cli/struct.Options.html\" title=\"struct deviantart_cli::Options\">Options</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
