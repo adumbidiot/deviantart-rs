2d1a7e5cd3c9d8d271e3c8c6031b564521db917e
adumbidiot <adumbidiot@users.noreply.github.com>
2024-07-01 23:14:35 +0000

    deploy: 82a153d4b7967a61877d71bd6420819945987a3e

---
+++ src-files.js
@@ -0,0 +1,5 @@
+var srcIndex = new Map(JSON.parse('[\
+["deviantart",["",[["types",[],["deviation.rs","oembed.rs","scraped_stash_info.rs","scraped_webpage_info.rs"]]],["client.rs","lib.rs","types.rs"]]],\
+["deviantart_cli",["",[["commands",[],["download.rs","login.rs","search.rs"]]],["commands.rs","config.rs","main.rs","util.rs"]]]\
+]'));
+createSrcSidebar();
---
+++ trait.impl/argh/trait.SubCommands.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"deviantart_cli":[["impl SubCommands for <a class=\"enum\" href=\"deviantart_cli/enum.SubCommand.html\" title=\"enum deviantart_cli::SubCommand\">SubCommand</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
