16b9e3baa519dcb61acbad0dcc895a27ec19c43c
adumbidiot <adumbidiot@users.noreply.github.com>
2024-08-01 20:55:32 +0000

    deploy: b60c1df8d5943194fe714f7901c8af5574b0883e

---
+++ trait.impl/argh/trait.SubCommand.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"deviantart_cli":[["impl SubCommand for <a class=\"struct\" href=\"deviantart_cli/commands/download/struct.Options.html\" title=\"struct deviantart_cli::commands::download::Options\">Options</a>"],["impl SubCommand for <a class=\"struct\" href=\"deviantart_cli/commands/login/struct.Options.html\" title=\"struct deviantart_cli::commands::login::Options\">Options</a>"],["impl SubCommand for <a class=\"struct\" href=\"deviantart_cli/commands/search/struct.Options.html\" title=\"struct deviantart_cli::commands::search::Options\">Options</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
