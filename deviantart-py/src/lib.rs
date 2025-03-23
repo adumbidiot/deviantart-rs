11a45e80663b9eae57dfff752b6857bd8025fb2d
Nathaniel Daniel <nathaniel.daniel23@outlook.com>
2025-03-22 21:14:56 -0700

    Add fetch by id support

--- deviantart-py/src/lib.rs
+++ deviantart-py/src/lib.rs
@@ -1,5 +1,8 @@
 use pyo3::exceptions::PyRuntimeError;
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
+use pyo3::types::PyString;
+use pyo3::types::PyStringMethods;
 use std::sync::LazyLock;
 
 static TOKIO_RT: LazyLock<std::io::Result<tokio::runtime::Runtime>> = LazyLock::new(|| {
@@ -22,13 +25,23 @@ impl Client {
         }
     }
 
-    pub fn get_deviation(&self, url: String) -> PyResult<Deviation> {
+    pub fn get_deviation(&self, source: Bound<'_, PyAny>) -> PyResult<Deviation> {
         let tokio_rt = TOKIO_RT
             .as_ref()
             .map_err(|error| PyRuntimeError::new_err(error.to_string()))?;
 
+        let source = if let Ok(url) = source.downcast::<PyString>() {
+            url.to_cow()?.into_owned()
+        } else if let Ok(id) = source.extract::<u64>() {
+            format!("https://www.deviantart.com/view/{id}")
+        } else {
+            return Err(PyValueError::new_err(
+                "source must be a deviation id or a url",
+            ));
+        };
+
         let scraped_webpage_info = tokio_rt
-            .block_on(async { self.client.scrape_webpage(url.as_str()).await })
+            .block_on(async { self.client.scrape_webpage(source.as_str()).await })
             .map_err(|error| PyRuntimeError::new_err(error.to_string()))?;
 
         let current_deviation = scraped_webpage_info
@@ -56,25 +69,42 @@ impl Default for Client {
 }
 
 #[pyclass]
+#[derive(serde::Serialize, serde::Deserialize)]
 pub struct Deviation {
-    #[pyo3(get)]
+    #[pyo3(set, get)]
     pub id: u64,
 
-    #[pyo3(get)]
+    #[pyo3(set, get)]
     pub title: String,
 
-    #[pyo3(get)]
+    #[pyo3(set, get)]
     pub description: Option<String>,
 
-    #[pyo3(get, name = "type")]
+    #[pyo3(set, get, name = "type")]
     pub kind: String,
 
-    #[pyo3(get)]
+    #[pyo3(set, get)]
     pub download_url: Option<String>,
 }
 
 #[pymethods]
 impl Deviation {
+    /// Dump this to a json string.
+    ///
+    /// Be very careful about using this for caching.
+    /// The embedded download urls can and will expire.
+    pub fn to_json(&self) -> PyResult<String> {
+        serde_json::to_string(&self).map_err(|error| PyRuntimeError::new_err(error.to_string()))
+    }
+
+    /// Parse this from a json string.
+    ///
+    /// Be very careful about using this for caching.
+    /// The embedded download urls can and will expire.
+    pub fn from_json(&self, value: &str) -> PyResult<String> {
+        serde_json::from_str(value).map_err(|error| PyRuntimeError::new_err(error.to_string()))
+    }
+
     pub fn __repr__(&self) -> String {
         let id = &self.id;
         let kind = &self.kind;
