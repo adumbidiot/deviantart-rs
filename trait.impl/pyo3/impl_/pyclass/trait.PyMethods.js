25536ae9dd6b067959cf0e98015efa8972439e9e
adumbidiot <adumbidiot@users.noreply.github.com>
2025-04-02 06:24:41 +0000

    deploy: 4df85fe04236b803fec8b54e1da816903c972ac9

---
+++ trait.impl/pyo3/impl_/pyclass/trait.PyMethods.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["deviantart_py",[["impl PyMethods&lt;<a class=\"struct\" href=\"deviantart_py/struct.Client.html\" title=\"struct deviantart_py::Client\">Client</a>&gt; for PyClassImplCollector&lt;<a class=\"struct\" href=\"deviantart_py/struct.Client.html\" title=\"struct deviantart_py::Client\">Client</a>&gt;"],["impl PyMethods&lt;<a class=\"struct\" href=\"deviantart_py/struct.Deviation.html\" title=\"struct deviantart_py::Deviation\">Deviation</a>&gt; for PyClassImplCollector&lt;<a class=\"struct\" href=\"deviantart_py/struct.Deviation.html\" title=\"struct deviantart_py::Deviation\">Deviation</a>&gt;"],["impl PyMethods&lt;<a class=\"struct\" href=\"deviantart_py/struct.Folder.html\" title=\"struct deviantart_py::Folder\">Folder</a>&gt; for PyClassImplCollector&lt;<a class=\"struct\" href=\"deviantart_py/struct.Folder.html\" title=\"struct deviantart_py::Folder\">Folder</a>&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[883]}
