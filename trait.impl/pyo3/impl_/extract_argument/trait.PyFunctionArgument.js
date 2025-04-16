cfb0d77ff2c4e11798ece0952c7c08985aa548f8
adumbidiot <adumbidiot@users.noreply.github.com>
2025-04-16 06:16:35 +0000

    deploy: 88111d1c86f4aac8149f482d6565e9a1e6fe4906

---
+++ trait.impl/pyo3/impl_/extract_argument/trait.PyFunctionArgument.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["deviantart_py",[["impl&lt;'a, 'py&gt; PyFunctionArgument&lt;'a, 'py, false&gt; for &amp;'a <a class=\"struct\" href=\"deviantart_py/struct.Client.html\" title=\"struct deviantart_py::Client\">Client</a>"],["impl&lt;'a, 'py&gt; PyFunctionArgument&lt;'a, 'py, false&gt; for &amp;'a <a class=\"struct\" href=\"deviantart_py/struct.Deviation.html\" title=\"struct deviantart_py::Deviation\">Deviation</a>"],["impl&lt;'a, 'py&gt; PyFunctionArgument&lt;'a, 'py, false&gt; for &amp;'a <a class=\"struct\" href=\"deviantart_py/struct.Folder.html\" title=\"struct deviantart_py::Folder\">Folder</a>"],["impl&lt;'a, 'py&gt; PyFunctionArgument&lt;'a, 'py, false&gt; for &amp;'a mut <a class=\"struct\" href=\"deviantart_py/struct.Client.html\" title=\"struct deviantart_py::Client\">Client</a>"],["impl&lt;'a, 'py&gt; PyFunctionArgument&lt;'a, 'py, false&gt; for &amp;'a mut <a class=\"struct\" href=\"deviantart_py/struct.Deviation.html\" title=\"struct deviantart_py::Deviation\">Deviation</a>"],["impl&lt;'a, 'py&gt; PyFunctionArgument&lt;'a, 'py, false&gt; for &amp;'a mut <a class=\"struct\" href=\"deviantart_py/struct.Folder.html\" title=\"struct deviantart_py::Folder\">Folder</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1183]}
---
+++ trait.impl/pyo3/impl_/pyclass/trait.PyClassNewTextSignature.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["deviantart_py",[["impl PyClassNewTextSignature&lt;<a class=\"struct\" href=\"deviantart_py/struct.Client.html\" title=\"struct deviantart_py::Client\">Client</a>&gt; for PyClassImplCollector&lt;<a class=\"struct\" href=\"deviantart_py/struct.Client.html\" title=\"struct deviantart_py::Client\">Client</a>&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[315]}
