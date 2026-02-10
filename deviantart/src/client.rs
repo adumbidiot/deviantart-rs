5b2a03bb32bf5340c829ff3bdd37b941a876209d
Nathaniel Daniel <nathaniel.daniel23@outlook.com>
2026-02-10 03:01:04 -0800

    Update user agent

--- deviantart/src/client.rs
+++ deviantart/src/client.rs
@@ -10,7 +10,7 @@ use reqwest_cookie_store::CookieStoreMutex;
 use std::sync::Arc;
 use url::Url;
 
-const USER_AGENT_STR: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36";
+const USER_AGENT_STR: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36";
 static ACCEPT_LANGUAGE_VALUE: HeaderValue = HeaderValue::from_static("en,en-US;q=0,5");
 static ACCEPT_VALUE: HeaderValue = HeaderValue::from_static("*/*");
 static REFERER_VALUE: HeaderValue = HeaderValue::from_static(HOME_URL);
