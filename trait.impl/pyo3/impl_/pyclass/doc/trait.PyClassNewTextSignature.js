eccff91176aaf0743dab882fef6d8d707c4629b4
adumbidiot <adumbidiot@users.noreply.github.com>
2026-06-12 19:51:58 +0000

    deploy: 3ceaac26872bb09286d5d54e5b9fc925c9ce424d

---
+++ trait.impl/pyo3/impl_/pyclass/doc/trait.PyClassNewTextSignature.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["deviantart_py",[["impl PyClassNewTextSignature for <a class=\"struct\" href=\"deviantart_py/struct.Client.html\" title=\"struct deviantart_py::Client\">Client</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[170]}
